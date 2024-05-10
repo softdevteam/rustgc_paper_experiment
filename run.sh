cf028d533effb2f0c3344955875df31fa87516aa
Jake Hughes <jake@bencher16.soft-dev.org>
2024-05-10 16:38:33 +0100

    Add barriers to runner

--- run.sh
+++ run.sh
@@ -8,20 +8,21 @@ cd som-rs
 # Perf
 git checkout -- .
 git apply ../configs/som-rs/rebench_perf.patch
-rebench --experiment all rebench.conf all
+rebench --experiment all rebench.conf all || true
 mv rebench.data ../raw_data/som-rs-perf.data
 
-# # Finaliser elision
+# Finaliser elision
 git checkout -- .
 git apply ../configs/som-rs/rebench_finaliser_elision.patch
 mkdir -p naive_counts
 mkdir -p elision_counts
-rebench --experiment all rebench.conf all
+rebench --experiment all rebench.conf all || true
 mv rebench.data ../raw_data/som-rs-finaliser_elision.data
 mv naive_counts ../raw_data/
 mv elision_counts ../raw_data/
 
 # Earlier finalisation / barriers
+git checkout -- .
 git apply ../configs/som-rs/rebench_barriers.patch
-rebench --experiment all rebench.conf all
+rebench --experiment all rebench.conf all || true
 mv rebench.data ../raw_data/som-rs-barriers.data
