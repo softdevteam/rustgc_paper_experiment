e6cb26e60e9f82abec9479e4ce26954c53092066
Jake Hughes <jh@jakehughes.uk>
2024-05-10 13:15:03 +0100

    Add som-rs benchmark runner

---
+++ run.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+set -e
+
+mkdir -p raw_data
+# =========== Run som-rs experiments ========
+cd som-rs
+# Perf
+git checkout -- .
+git apply ../configs/som-rs/rebench_perf.patch
+rebench --experiment all rebench.conf all
+mv rebench.data ../raw_data/som-rs-perf.data
+
+# # Finaliser elision
+git checkout -- .
+git apply ../configs/som-rs/rebench_finaliser_elision.patch
+mkdir -p naive_counts
+mkdir -p elision_counts
+rebench --experiment all rebench.conf all
+mv rebench.data ../raw_data/som-rs-finaliser_elision.data
+mv naive_counts ../raw_data/
+mv elision_counts ../raw_data/
+
+# Earlier finalisation / barriers
+git apply ../configs/som-rs/rebench_barriers.patch
+rebench --experiment all rebench.conf all
+mv rebench.data ../raw_data/som-rs-barriers.data
