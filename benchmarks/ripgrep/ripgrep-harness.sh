599175ef3745909063c07ecd7a68c516018c51fc
Jake Hughes <jh@jakehughes.uk>
2025-03-21 13:23:26 +0000

    Add ripgrep benchmarks

---
+++ benchmarks/ripgrep/ripgrep-harness.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export PATH="$(dirname $0)/:$PATH"
+CMD_DIR="benchmarks/ripgrep/commands"
+LINUX="benchmarks/ripgrep/linux"
+
+CMD=$(<$CMD_DIR/$@)
+
+if [ -z "${HT}" ]; then
+    $CMD $LINUX
+else
+    $HT $CMD $LINUX
+fi
