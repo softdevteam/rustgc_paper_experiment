7b893630c2e1fab8d4f7252d3e3269f89fe4ea95
Jake Hughes <jh@jakehughes.uk>
2025-06-25 09:18:22 +0100

    Update the rebench alloy_adapter to work in a Docker image

--- alloy_adapter.py
+++ alloy_adapter.py
@@ -15,9 +15,9 @@ from rebench.model.measurement import Measurement
 class AlloyAdapter(GaugeAdapter):
     # To be sure about how to parse the output, give custom format
     # This avoids issues with perhaps none-standard /bin/usr/time
-    time_format = '"max rss (kb): %M\nwall-time (secounds): %e\n"'
-    re_formatted_time = re.compile(r"^wall-time \(secounds\): (\d+\.\d+)")
-    re_formatted_rss = re.compile(r"^max rss \(kb\): (\d+)")
+    time_format = '"usr: %U\nwall-time: %e\n"'
+    re_formatted_time = re.compile(r"^wall-time: (\d+\.\d+)")
+    re_formatted_usr = re.compile(r"^usr: (\d+\.\d+)")
 
     _completed_time_availability_check = False
     _use_formatted_time = False
@@ -30,12 +30,7 @@ class AlloyAdapter(GaugeAdapter):
         GaugeAdapter.__init__(self, include_faulty, executor)
 
     def _create_command(self, command):
-        if self._use_formatted_time:
-            return "%s -f %s %s" % (self._time_bin, AlloyAdapter.time_format, command)
-        else:
-            # use standard, but without info on memory
-            # TODO: add support for reading out memory info on OS X
-            return "/usr/bin/time -p %s" % command
+        return "%s -f %s %s" % (self._time_bin, AlloyAdapter.time_format, command)
 
     def should_enable_premature_finalizer_optimization(self, run_id):
         exec = run_id.benchmark.suite.executor.name
@@ -55,8 +50,11 @@ class AlloyAdapter(GaugeAdapter):
         return "gcvs" in exec or "premopt" in exec or "elision-opt" in exec
 
     def setup_stats(self, run_id):
-        run_id.env["ALLOY_LOG"] = self.alloy_stats.name
-        run_id.env["DISPLAY"] = ":99"
+        bm = run_id.benchmark.name.lower()
+        if "GC_LOG_DIR" in run_id.env:
+            logdir = run_id.env["GC_LOG_DIR"]
+            run_id.env["GC_LOG_FILE"] = f"{logdir}-{bm}.csv"
+        # run_id.env["DISPLAY"] = ":99"
 
     def acquire_command(self, run_id):
         self.setup_stats(run_id)
@@ -100,39 +98,11 @@ class AlloyAdapter(GaugeAdapter):
         AlloyAdapter._time_bin = time_bin
         AlloyAdapter._completed_time_availability_check = True
 
-    def parse_stats(self, run_id, invocation, iteration, current):
-        df = pd.read_csv(self.alloy_stats.name)
-        if df.empty:
-            print("The file is empty (no data rows).")
-        else:
-            stats = df.iloc[0]
-            assert (
-                self.should_enable_finalizer_elision(run_id) == stats["elision enabled"]
-            )
-            assert (
-                self.should_enable_premature_finalizer_prevention(run_id)
-                == stats["pfp enabled"]
-            )
-            assert (
-                self.should_enable_premature_finalizer_optimization(run_id)
-                == stats["premopt enabled"]
-            )
-            stats = stats[3:]
-            for idx, value in stats.items():
-                measure = Measurement(
-                    invocation,
-                    iteration,
-                    float(value),
-                    "stat",
-                    run_id,
-                    idx,
-                )
-                current.add_measurement(measure)
-
     def parse_data(self, data, run_id, invocation):
         iteration = 1
         data_points = []
         current = DataPoint(run_id)
+        total_measure = None
 
         for line in data.split("\n"):
             if self.check_for_error(line):
@@ -140,20 +110,25 @@ class AlloyAdapter(GaugeAdapter):
                     "Output of bench program indicated error."
                 )
 
-            if self._use_formatted_time:
-                match = self.re_formatted_time.match(line)
-                if match:
-                    self.parse_stats(run_id, invocation, iteration, current)
-                    time = float(match.group(1)) * 1000
-                    measure = Measurement(
-                        invocation, iteration, time, "ms", run_id, "total"
-                    )
-                    current.add_measurement(measure)
-                    data_points.append(current)
-                    current = DataPoint(run_id)
-                    iteration += 1
-            else:
-                raise NotImplementedError
+            match1 = self.re_formatted_usr.match(line)
+            match2 = self.re_formatted_time.match(line)
+            if match1:
+                usr = float(match1.group(1)) * 1000
+                measure = Measurement(invocation, iteration, usr, "ms", run_id, "usr")
+                current.add_measurement(measure)
+            elif match2:
+                time = float(match2.group(1)) * 1000
+                measure = Measurement(
+                    invocation, iteration, time, "ms", run_id, "total"
+                )
+                current.add_measurement(measure)
+                data_points.append(current)
+                current = DataPoint(run_id)
+                iteration += 1
+
+        if total_measure:
+            current.add_measurement(total_measure)
+            data_points.append(current)
 
         if not data_points:
             raise OutputNotParseable(data)
